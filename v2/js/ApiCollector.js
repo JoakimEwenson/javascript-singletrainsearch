b1097186e1b2b6924103db1c86dc9809500e4d5a
Joakim Ewenson <joakim@ewenson.se>
2019-08-12 11:42:47 +0200

    WIP

--- v2/js/ApiCollector.js
+++ v2/js/ApiCollector.js
@@ -1,29 +1,16 @@
-function ApiCollector() {
-    var apiKey = "dfc3b8374d774d5e94655bcd32d7c5c3";
-
-    var data = "<REQUEST>" +
-            "<LOGIN authenticationkey='" + apiKey + "' />" +
-            "<QUERY objecttype='TrainStation' schemaversion='1' limit='10'>" +
-            "<FILTER>" +
-            "</FILTER>" +
-            "<INCLUDE>AdvertisedLocationName</INCLUDE>" +
-            "<INCLUDE>LocationSignature</INCLUDE>" +
-            "</QUERY>" +
-        "</REQUEST>";
-
+function ApiCollector(returnType,data) {
     var xhr = new XMLHttpRequest();
     xhr.withCredentials = false;
 
     xhr.addEventListener("readystatechange", function () {
         if (this.readyState === 4) {
-            console.log(this.responseText);
-            /*
-            var obj = JSON.parse(this.responseText);
-            for (var i in obj.RESPONSE.RESULT[0].TrainStation) {
-                //console.log(obj.RESPONSE.RESULT[0].TrainStation[i].LocationSignature + " = " + obj.RESPONSE.RESULT[0].TrainStation[i].AdvertisedLocationName);
-                output += obj.RESPONSE.RESULT[0].TrainStation[i].LocationSignature + " = " + obj.RESPONSE.RESULT[0].TrainStation[i].AdvertisedLocationName + "\n";
+            //console.log(this.responseText);
+            if (returnType == "stationNames") {
+                document.getElementById("temporaryContent").innerHTML = renderStationNameList(JSON.parse(this.responseText));
             }
-            */
+            if (returnType == "singleTrainState") {
+                document.getElementById("temporaryContent").innerHTML = renderSingleTrainState(JSON.parse(this.responseText));
+            }
         }
     });
 
@@ -33,4 +20,45 @@ function ApiCollector() {
     xhr.send(data);
 
     return xhr.responseText;
+}
+
+function renderStationNameList(obj) {
+    output = "<table class='w3-table w3-bordered w3-striped'>";
+    output += "<thead>";
+    output += "<tr>";
+    output += "<th>LocationSignature</th>";
+    output += "<th>LocationName</th>"
+    output += "</tr>";
+    output += "</thead>";
+    for (var i in obj.RESPONSE.RESULT[0].TrainStation) {
+        output += "<tr>";
+        output += "<td>" + obj.RESPONSE.RESULT[0].TrainStation[i].LocationSignature + "</td><td>" + obj.RESPONSE.RESULT[0].TrainStation[i].AdvertisedLocationName + "</td>";
+        output += "</tr>";
+    }
+    output += "</table>";
+
+    return output;
+}
+
+function renderSingleTrainState(obj) {
+    output = "<table class='w3-table w3-bordered w3-striped'>";
+    output += "<thead>";
+    output += "<tr>";
+    output += "<th>LocationSignature</th>";
+    output += "<th>AdvertisedTimeAtLocation</th>"
+    output += "<th>TimeAtLocation</th>"
+    output += "</tr>";
+    output += "</thead>";
+    output += "<tbody>";
+    for (var i in obj.RESPONSE.RESULT[0].TrainAnnouncement) {
+        output += "<tr>";
+        output += "<td>" + obj.RESPONSE.RESULT[0].TrainAnnouncement[i].LocationSignature + "</td>"
+        output += "<td>" + new Date(obj.RESPONSE.RESULT[0].TrainAnnouncement[i].AdvertisedTimeAtLocation).toLocaleTimeString("sv-SE") + "</td>";
+        output += "<td>" + new Date(obj.RESPONSE.RESULT[0].TrainAnnouncement[i].TimeAtLocation).toLocaleTimeString("sv-SE") + "</td>";
+        output += "</tr>";
+    }
+    output += "</tbody>";
+    output += "</table>";
+
+    return output;
 }
